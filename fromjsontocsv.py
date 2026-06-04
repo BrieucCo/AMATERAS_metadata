c7f53cb9860e4e9a012644f1821c579fae1d2129
BrieucCo <brieuc.collet@gmail.com>
2026-06-04 11:55:42 +0200

    change fromjson routine location to fromjson py file. Adapts this routine to subfolder (high)

--- fromjsontocsv.py
+++ fromjsontocsv.py
@@ -1,5 +1,37 @@
-from create_amateras_low_metadata import fromjsontocsv
+import os
+import csv
+import json
+from pathlib import Path
+
 from sys import argv
+
+
+def fromjsontocsv(metadatafolder="./lowmetadata/", csvfile="./lowmetadata.csv"):
+    """
+    Reads every json file in folder to make a csv file from.
+    """
+    if not os.path.isdir(metadatafolder):
+        print("Not a folder")
+        return None
+
+    metadatafolder, csvfile = Path(metadatafolder), Path(csvfile)
+
+    starting = True
+    list_folder = list(metadatafolder.rglob("*"))
+
+    with open(csvfile, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for e in list_folder:
+            if e.suffix == "fits":
+                print(e)
+                with open(e.as_posix(), 'r') as file:
+                    data = json.load(file)
+                    if starting:
+                        writer.writerows([data.keys()])
+                        starting = False
+                    writer.writerows([data.values()])
+    print("Finished")
+
 
 if __name__ == "__main__":
     argv
